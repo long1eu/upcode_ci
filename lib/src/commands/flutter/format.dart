1ae7919e82dcfc38c26462865621df2bd92fbecb
Cosmin <cosmin@upcode.dev>
2025-03-10 17:02:34 +0200

    Add the "module" parameter back for format command

--- lib/src/commands/flutter/format.dart
+++ lib/src/commands/flutter/format.dart
@@ -11,12 +11,17 @@ import 'package:upcode_ci/src/commands/command.dart';
 
 class FlutterFormatCommand extends UpcodeCommand {
   FlutterFormatCommand(Map<String, dynamic> config) : super(config) {
-    argParser.addFlag(
-      'modify',
-      defaultsTo: false,
-      help:
-          'If false it will only check if there are changes that need to be done and exists with non 0 code if so. If true it will format the code.',
-    );
+    argParser
+      ..addFlag(
+        'modify',
+        defaultsTo: false,
+        help:
+            'If false it will only check if there are changes that need to be done and exists with non 0 code if so. If true it will format the code.',
+      )
+      ..addMultiOption(
+        'module',
+        help: 'Select what modules you want to check.',
+      );
   }
 
   @override
@@ -29,7 +34,14 @@ class FlutterFormatCommand extends UpcodeCommand {
   FutureOr<dynamic> run() async {
     final bool modify = argResults!['modify'] ?? false;
 
-    for (final String module in formattedModules) {
+    List<String> modules;
+    if (argResults!.wasParsed('module')) {
+      modules = List<String>.from(argResults!['module']).map((String item) => item.dirName).toList();
+    } else {
+      modules = formattedModules;
+    }
+
+    for (final String module in modules) {
       final List<String> files = Directory(module)
           .listSync(recursive: true, followLinks: false)
           .whereType<File>()
